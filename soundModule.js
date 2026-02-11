645003924e71d63fb2008f6b7c813f47e87aa009
Tess <146678224+tessellis@users.noreply.github.com>
2026-02-10 22:47:45 -0800

    Revert "Improved Top Menu Version 1"

--- soundModule.js
+++ soundModule.js
@@ -10,7 +10,7 @@ function createSoundModuleTemplate(moduleId) {
                 <div class="moduleTopOptions">
                     <div class="volumeContainer">
                         <label for="volume">Volume:</label>
-                        <input type="range" class="volume" min="-60" max="3">
+                        <input type="range" class="volume" min="-60" max="3" value="-10">
                     </div>
                 </div>
 
@@ -30,7 +30,7 @@ function createSoundModuleTemplate(moduleId) {
                 
                 <!-- Collapse button -->
                 <button class="collapse-btn"> 
-                Sound Options <span class="arrow-icon">▼</span>
+                More Options <span class="arrow-icon">▼</span>
                 </button>
                 
                 <!-- Bottom options (collapsible) -->
